86c99f2c6a99ef08722884e2372a5dadb48f143f
KubaKajderowicz <ksimulations@hotmail.com>
2019-10-02 19:37:53 +0200

    Uitwerking van de exercise

---
+++ game.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <h2>Steen Papier Schaar</h2>
+
+    <h3>Speler 1: Papier</h3>
+
+    <h4>Speler 2:</h4>
+    <form action="" method="POST">
+        <select>
+            <option value="steen">steen</option>
+            <option value="papier">papier</option>
+            <option value="schaar">schaar</option>
+        </select>
+        <br>
+        <input type="submit" value="selecteer">
+    </form>
+</body>
+
+</html>
